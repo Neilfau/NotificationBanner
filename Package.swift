d43af7579fd490eb53add705c94c391dfcc3b652
Neil Faulkner <neilfau@icloud.com>
2024-08-28 09:25:34 +0200

    - Changed dependency to close for Snapkit Bug

--- Package.swift
+++ Package.swift
@@ -12,13 +12,18 @@ let package = Package(
                  targets: ["NotificationBannerSwift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.7.1"),
+        //.package(url: "https://github.com/SnapKit/SnapKit", from: "5.7.1"),
+        .package(url: "https://github.com/close-dev-team/mobile-close-channel-sdk-ios-dependencies", from: "1.7.0"),
         .package(url: "https://github.com/cbpowell/MarqueeLabel", from: "4.0.1")
     ],
     targets: [
         .target(
             name: "NotificationBannerSwift",
-            dependencies: [.byName(name: "MarqueeLabel"), .byName(name: "SnapKit")],
+            dependencies: [
+                .byName(name: "MarqueeLabel"),
+                .product(name: "CloseChannelDependencies", package: "mobile-close-channel-sdk-ios-dependencies"),
+                //.byName(name: "SnapKit")
+            ],
             path: "NotificationBanner/Classes"
         )
     ],
